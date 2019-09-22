3ec6278c9d4cb724522a42798face92e23594fa7
MrKaliostro <49442839+MrKaliostro@users.noreply.github.com>
2019-09-22 16:05:52 +0300

    Update hw1_invertint.py

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,5 @@
 
 
 def reverse(number):
-    '''
-    Метод, принимающий на вход int и
-    возвращающий инвертированный int
-    :param number: исходное число
-    :return: инвертированное число
-    '''
+    return str(number)[::-1]
     raise NotImplementedError
